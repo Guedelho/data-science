9b5139cb828d7ab3a41d5e7ca0501f297fadac7a
Guedelho <mateusguedelho@yahoo.com.br>
2019-07-24 20:36:50 -0300

    simple median and distplot

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 notas = pd.read_csv("./ml-latest-small/ratings.csv")
-print(notas.head())
+filmes = pd.read_csv("./ml-latest-small/movies.csv")
+notas.columns = ['usuarioId', 'filmeId', 'nota', 'momento']
+filmes.columns = ['filmeId', 'titulo', 'generos']
 
-notas.rating.plot(kind='hist')
+print(notas.head())
+print(filmes.head())
+
+notas.nota.describe()
+
+notas.nota.plot(kind='hist')
 plt.show()
+
+medias_por_filme = notas.groupby('filmeId').mean()['nota']
+
+sns.distplot(medias_por_filme)
+plt.title('histograma das medias dos filmes')
+plt.show()
+
+sns.boxplot(y=medias_por_filme)
+plt.show()
+
